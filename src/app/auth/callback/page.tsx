ac4289ac67fcebfcaaaa94af9f1969a9777ce131
Prince3255 <princesuthar897@gmail.com>
2025-08-06 20:07:43 +0530

    update index

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,6 +1,8 @@
 import { onAuthenticateUser } from '@/app/action/user' 
 import { redirect } from 'next/navigation'
 
+export const dynamic = 'force-dynamic'
+
 type Props = {}
 
 const AuthCallbackPage = async (props: Props) => {
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,8 @@ import { redirect } from 'next/navigation'
 
 type Props = {}
 
+export const dynamic = 'force-dynamic'
+
 const Dashboard = async (props: Props) => {
 
     const auth = await onAuthenticateUser()
